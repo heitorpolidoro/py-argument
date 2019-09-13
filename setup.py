d3981fbea7f41942c3300c5d4105d8dd21c60012
Heitor Polidoro <heitor.polidoro@redealumni.com>
2019-09-13 17:57:02 -0300

    setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='argument',
+    version='0.0.1',
+    description='Package to create command line arguments for Python.',
+    url='git@github.com:heitorpolidoro/py-argument-utils.git',
+    author='Heitor Polidoro',
+    # author_email='heitor.polidoro',
+    license='unlicense',
+    packages=['argument'],
+    zip_safe=False
+)
