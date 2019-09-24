fc039d1d7c5b4ad79832134250cd990efc87f612
Heitor Polidoro <heitor.polidoro@redealumni.com>
2019-09-24 16:11:01 -0300

    @Command

--- argument/__init__.py
+++ argument/__init__.py
@@ -1,5 +1,6 @@
 from argument.argument import Argument
 from argument.argument_parser import ArgumentParser
+from argument.command import Command
 
 name = 'argument'
-version = '1.0.0'
+version = '1.1.0'
--- argument/argument.py
+++ argument/argument.py
@@ -9,17 +9,30 @@ class Argument(object):
     """
     arguments = []
 
-    def __init__(self, method, **kwargs):
+    def __init__(self, method=None, help=None, **_):
+        self.help = help
         self.method = method
+        # if method is None the decorator has parameters
+        if method is not None:
+            self.args = tuple(['--' + method.__name__])
 
-        self.args = tuple(['--' + method.__name__])
+            self.kwargs = {
+                'action': CustomAction,
+                'method': method,
+                'help': help
+            }
+            parameters = inspect.signature(method).parameters
+            # nargs = number of arguments in method
+            self.kwargs['nargs'] = len(parameters)
+            if parameters:
+                self.kwargs['metavar'] = list(parameters.values())[0].name
 
-        self.kwargs = kwargs
-        self.kwargs['action'] = CustomAction
-        self.kwargs['method'] = method
-        self.kwargs.setdefault('nargs', len(inspect.signature(method).parameters))
-
-        self.arguments.append(self)
+            self.arguments.append(self)
 
     def __call__(self, *args, **kwargs):
+        # if method is None the decorator has parameters and the first argument is the method
+        if self.method is None:
+            self.method = list(args)[0]
+            self.__init__(**vars(self))
+            return self
         return self.method(*args, **kwargs)
